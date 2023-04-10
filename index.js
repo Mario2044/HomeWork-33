7a3aae3df4d45b564d4197f88876f341cc9dcb41
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-04-10 20:52:38 +0300

    Update file js

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-class Hamburger {
-    static SIZE_SMALL = {price: 50, calories: 20};
-    static SIZE_BIG = {price: 100, calories: 40};
-    static STUFFING_CHEESE = {price: 10, calories: 20};
-    static STUFFING_SALAD = {price: 20, calories: 5};
-    static STUFFING_POTATO = {price: 15, calories: 10};
-    static TOPPING_MAYO = {price: 20, calories: 5};
-    static TOPPING_SAUCE = {price: 15, calories: 0};
-  
-    constructor(size, stuffing) {
-      this.size = size;
-      this.stuffing = stuffing;
-      this.toppings = [];
-    }
-  
-    addTopping(topping) {
-      this.toppings.push(topping);
-    }
-  
-    calculatePrice() {
-      const toppingsPrice = this.toppings.reduce((acc, topping) => acc + topping.price, 0);
-      return this.size.price + this.stuffing.price + toppingsPrice;
-    }
-  
-    calculateCalories() {
-      const toppingsCalories = this.toppings.reduce((acc, topping) => acc + topping.calories, 0);
-      return this.size.calories + this.stuffing.calories + toppingsCalories;
-    }
+class Humburger {
+  constructor(size, stuffing) {
+    this.size = size;
+    this.stuffing = stuffing;
+    this.toppings = [];
+  }
+
+  static SIZE_SMALL = {price: 50, calories: 20};
+  static SIZE_LARGE = {price: 100, calories: 40};
+  static STUFFING_CHEESE = {price: 15, calories: 25};
+  static STUFFING_SALAD = {price: 15, calories: 5};
+  static STUFFING_POTATO = {price: 10, calories: 20};
+  static TOPPING_MAYO = {price: 20, calories: 10};
+  static TOPPING_SPICE = {price: 15, calories: 0};
+
+  addTopping(topping) {
+    this.toppings.push(topping);
+  }
+
+  calculatePrice() {
+    let price = this.size.price + this.stuffing.price;
+    this.toppings.forEach(topping => price += topping.price);
+    return price;
+  }
+
+  calculateCalories() {
+    let calories = this.size.calories + this.stuffing.calories;
+    this.toppings.forEach(topping => calories += topping.calories);
+    return calories;
+  }
 }
-  
-const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
-hamburger.addTopping(Hamburger.TOPPING_MAYO);
-console.log("Calories: " + hamburger.calculateCalories());
-console.log("Price: " + hamburger.calculatePrice());
-hamburger.addTopping(Hamburger.TOPPING_SAUCE);
-console.log("Price with sauce: " + hamburger.calculatePrice());
+
+let humburger = new Humburger(Humburger.SIZE_LARGE, Humburger.STUFFING_CHEESE);
+humburger.addTopping(Humburger.TOPPING_MAYO);
+console.log('Calories: ' + humburger.calculateCalories());
+console.log('Price: ' + humburger.calculatePrice());
+humburger.addTopping(Humburger.TOPPING_SPICE)
+console.log('Price with sauce:' + humburger.calculatePrice());
